3689da943045a90b19e4ed5c7889038710429a90
Ryan Hemmila <rhemmila@wisc.edu>
2021-09-26 16:19:11 -0500

    column definition changes

--- ZeusMigrations/Models/ColumnDefinition.cs
+++ ZeusMigrations/Models/ColumnDefinition.cs
@@ -1,6 +1,12 @@
-﻿namespace ZeusMigrations.Models {
+﻿using System.Text;
+
+namespace ZeusMigrations.Models {
 
   public class ColumnDefinition {
+
+    public Identity Identity { get; set; }
+    public bool IsPrimaryKey { get; set; }
+    public bool IsNotNull { get; set; }
 
     private SqlDataType _dataType;
     private string _columnName;
@@ -11,7 +17,16 @@
     }
 
     public override string ToString() {
-      return $"{this._columnName} {this._dataType}";
+      StringBuilder sql = new StringBuilder($"{this._columnName} {this._dataType}");
+      if (this.Identity != null) {
+        sql.Append($" {this.Identity}");
+      } else if (this.IsNotNull) {
+        sql.Append(" NOT NULL");
+      }
+      if (this.IsPrimaryKey) {
+        sql.Append(" PRIMARY KEY");
+      }
+      return sql.ToString();
     }
   }
 }
---
+++ ZeusMigrations/Models/Identity.cs
@@ -0,0 +1,23 @@
+﻿namespace ZeusMigrations.Models {
+
+  public class Identity {
+
+    public static Identity Default;
+
+    static Identity() {
+      Default = new Identity(1, 1);
+    }
+
+    private int _increment;
+    private int _seed;
+
+    public Identity(int seed, int increment) {
+      this._increment = increment;
+      this._seed = seed;
+    }
+
+    public override string ToString() {
+      return $"IDENTITY({this._seed},{this._increment})";
+    }
+  }
+}
