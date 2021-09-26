c1689a808574fceb36df43da6108609ca3a88243
Ryan Hemmila <rhemmila@wisc.edu>
2021-09-26 18:18:36 -0500

    fixes

--- ZeusMigrations/MigrationManager.cs
+++ ZeusMigrations/MigrationManager.cs
@@ -34,9 +34,14 @@ namespace ZeusMigrations {
                   command.Transaction = migrationTransaction;
                   command.ExecuteNonQuery();
                 }
-                InsertIntoMigrationTable(migration.Id);
               }
-              Console.WriteLine("\nSuccessfully completed all pending migrations. Database is up to date");
+              string query = "INSERT INTO migrations (migration_id) VALUES (@MigrationId);";
+              using (SqlCommand command = new SqlCommand(query, conn)) {
+                command.Parameters.AddWithValue("@MigrationId", migration.Id.ToString());
+                command.Transaction = migrationTransaction;
+                command.ExecuteNonQuery();
+              }
+              migrationTransaction.Commit();
             } catch {
               try {
                 Console.ForegroundColor = ConsoleColor.Red;
@@ -49,42 +54,49 @@ namespace ZeusMigrations {
                 Console.WriteLine("Failed to rollback transaction after migration failed");
               }
             }
+
           }
         }
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("\nSuccessfully completed all pending migrations. Database is up to date");
       }
     }
 
     private bool InsertIntoMigrationTable(Guid migrationId) {
+      bool result = false;
       using (SqlConnection conn = this.GetNewConnectionAndOpen()) {
         string query = "INSERT INTO migrations (migration_id) VALUES (@MigrationId);";
         using (SqlCommand command = new SqlCommand(query, conn)) {
           command.Parameters.AddWithValue("@MigrationId", migrationId.ToString());
-          return command.ExecuteNonQuery() == 1;
+          result = command.ExecuteNonQuery() == 1;
         }
       }
+      return result;
     }
 
     private HashSet<Guid> GetExecutedMigrations() {
       CreateMigrationTableIfNotExist();
 
+      string query = "SELECT migration_id FROM migrations;";
       HashSet<Guid> migrationIds = new HashSet<Guid>();
-      SqlDataReader reader = ExecuteReader("SELECT migration_id FROM migrations;");
-      while (reader.Read()) {
-        if (Guid.TryParse(reader.GetString(0), out Guid migrationId)) {
-          migrationIds.Add(migrationId);
+      using (SqlConnection conn = this.GetNewConnectionAndOpen()) {
+        using (SqlCommand command = new SqlCommand(query, conn)) {
+          SqlDataReader reader = command.ExecuteReader();
+          while (reader.Read()) {
+            if (Guid.TryParse(reader.GetString(0), out Guid migrationId)) {
+              migrationIds.Add(migrationId);
+            }
+          }
         }
       }
       return migrationIds;
     }
 
     private void CreateMigrationTableIfNotExist() {
-      ExecuteNonQuery("IF OBJECT_ID('migrations') IS NULL CREATE TABLE migrations (id int IDENTITY(1,1) PRIMARY KEY, migration_id varchar(40) NOT NULL);");
-    }
-
-    private SqlDataReader ExecuteReader(string query) {
+      string query = "IF OBJECT_ID('migrations') IS NULL CREATE TABLE migrations (id int IDENTITY(1,1) PRIMARY KEY, migration_id varchar(40) NOT NULL);";
       using (SqlConnection conn = this.GetNewConnectionAndOpen()) {
         using (SqlCommand command = new SqlCommand(query, conn)) {
-          return command.ExecuteReader();
+          command.ExecuteNonQuery();
         }
       }
     }
@@ -93,14 +105,6 @@ namespace ZeusMigrations {
       SqlConnection connection = new SqlConnection(this._databaseUrl);
       connection.Open();
       return connection;
-    }
-
-    private void ExecuteNonQuery(string query) {
-      using (SqlConnection conn = this.GetNewConnectionAndOpen()) {
-        using (SqlCommand command = new SqlCommand(query, conn)) {
-          command.ExecuteNonQuery();
-        }
-      }
     }
   }
 }
